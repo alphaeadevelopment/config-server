c5e603b459e6ab8a1065d235d65c7b3548d79f2b
Graham <gk@Grahams-iMac.local>
2018-01-21 16:18:14 +0000

    Allow cors origins by regex

--- src/main/java/uk/alphaea/dev/configserver/WebConfiguration.java
+++ src/main/java/uk/alphaea/dev/configserver/WebConfiguration.java
@@ -1,24 +1,56 @@
 package uk.alphaea.dev.configserver;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsProcessor;
+import org.springframework.web.cors.DefaultCorsProcessor;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebConfiguration {
+	
+	@Value("${cors.allowed.origins.regex}") String corsAllowedOriginsRegex;
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(
-						"http://localhost:3000",
-						"http://prr-test.alphaea.uk"
-						);
+	CorsProcessor corsProcessor() {
+		return new DefaultCorsProcessor();
+	}
+
+	@Bean
+	CorsConfiguration corsConfiguration() {
+		final Pattern p = Pattern.compile(corsAllowedOriginsRegex);
+
+		CorsConfiguration config = new CorsConfiguration() {
+			public String checkOrigin(String requestOrigin) {
+				if (!StringUtils.hasText(requestOrigin)) {
+					return null;
+				}
+				Matcher matcher = p.matcher(requestOrigin);
+				return (matcher.matches() ? requestOrigin : null);
 			}
 		};
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		return config;
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter(CorsConfiguration config, CorsProcessor corsProcessor) {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		CorsFilter corsFilter = new CorsFilter(source);
+		corsFilter.setCorsProcessor(corsProcessor);
+		FilterRegistrationBean bean = new FilterRegistrationBean(corsFilter);
+		bean.setOrder(0);
+		return bean;
 	}
 }
